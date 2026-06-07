4a6160e0793f4a882c2064651aabba5260d50881
Gautham Vanjre <vanjregautham@gmail.com>
2026-06-07 20:59:34 +0530

    feat: function for thumbnails of a job

--- backend/services/generator.py
+++ backend/services/generator.py
@@ -55,3 +55,41 @@ async def generate_single_thumbnail(thumbnail_id: str, prompt: str, headshot_url
             thumb.error_message = str(e)[:500]  # Truncate error message to fit in DB
             session.add(thumb)
             session.commit()
+
+async def process_job(job_id: str):
+    # mark job as processing
+    # wait for all workers to finish
+    # mark job as completed/failed based on thumbnail status
+    with Session(engine) as session:
+        job = session.get(Job, job_id)
+        job.status = "Processing"
+        prompt = job.prompt
+        headshot_url = job.headshot_url
+        session.add(job)
+        session.commit()
+
+        # find all thumbnails for this job
+        thumbnails_of_a_job = session.exec(
+            select(Thumbnail).where(Thumbnail.job_id == job_id)
+        ).all()
+        thumbnails_ids = [t.id for t in thumbnails_of_a_job]
+
+        # start one worker for each thumbnail
+        tasks = [
+            generate_single_thumbnail(tid, prompt, headshot_url)
+            for tid in thumbnails_ids
+        ]
+        # running all thumbmnails concurrently
+        # wait for all workers to finish
+        await asyncio.gather(*tasks, return_exceptions=True)
+
+        # mark job as completed/failed based on thumbnail status
+        with Session(engine) as session:
+            thumbnails = session.exec(
+                select(Thumbnail).where(Thumbnail.job_id == job_id)
+            ).all()
+            all_failed = all(t.status == "failed" for t in thumbnails)
+            job = session.get(Job, job_id)
+            job.status = "failed" if all_failed else "completed"
+            session.add(job)
+            session.commit()
